4d23ddbb6da1b7f5396463fcdb0d61b7afa9b6c6
rinorselmani9 <rs44583@ubt-uni.net>
2023-04-04 17:44:36 +0200

    created screens folder with the needed screens in it, added styles from styles folder

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Home from './screens/home';
-import * as Font from 'expo-font'
+import * as Font from 'expo-font';
+// import AppLoading from 'expo-app-loading'
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/static/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/static/Nunito-Bold.ttf'),
+})
 
 export default function App() {
-  return (
-    <Home/>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false)
 
+    return (
+      <Home />
+    )
+    
+}
